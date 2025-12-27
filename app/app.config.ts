7d42a4a17fcd76a024006f019c4447e24e37544d
Dan Farrow <dan@zellement.com>
2025-12-27 15:31:57 +0000

    start

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,48 @@
 export default defineAppConfig({
-    icon: {
-        size: '24px', // default <Icon> size applied
-        class: 'icon', // default <Icon> class applied
-        mode: 'css', // default <Icon> mode applied
-        cssLayer: 'base' // set the css layer to inject to
+    ui: {
+        colors: {
+            primary: 'navy',
+            secondary: 'butterscotch',
+            tertiary: 'tuscany',
+            info: 'blue',
+            error: 'red',
+            success: 'green'
+        },
+
+        card: {
+            slots: {
+                root: '!ring-0 card-border',
+                header: 'p-3 sm:p-3',
+                body: 'p-3 sm:p-3',
+                footer: 'p-3 sm:p-3'
+            }
+        },
+
+        slideover: {
+            slots: {
+                title: 'uc-text'
+            }
+        },
+
+        button: {
+            variants: {
+                size: {
+                    '2xs': 'px-1 gap-1 py-px text-2xs'
+                }
+            }
+        },
+        stepper: {
+            slots: {
+                trigger: 'size-4.5 m-0',
+                wrapper: 'm-0'
+            },
+            variants: {
+                size: {
+                    xs: {
+                        wrapper: '!m-0'
+                    }
+                }
+            }
+        }
     }
 })
--- app/components/Organisms/SiteHeader.vue
+++ app/components/Organisms/SiteHeader.vue
@@ -1,47 +1,7 @@
 <template>
-    <header
-        role="banner"
-        :class="headerClasses"
-        class="fixed top-0 right-0 left-0 z-50 w-full bg-white/80 py-4 transition-transform duration-500 lg:py-6 lg:backdrop-blur-sm"
-    >
+    <header role="banner">
         <div class="container grid-layout container-px">
-            <div
-                class="relative col-span-9 col-start-2 row-start-1 flex w-full items-center justify-between lg:col-span-10 lg:col-start-2"
-            >
-                <site-brand class="" />
-                <div
-                    class="fixed inset-0 z-10 grid h-screen w-full grid-flow-row auto-rows-max grid-cols-12 overflow-y-scroll bg-gray-100 py-4 transition-all duration-300 lg:static lg:inset-auto lg:z-auto lg:flex lg:h-auto lg:overflow-y-visible lg:bg-transparent lg:p-0"
-                    :class="mobileNavWrapperClasses"
-                >
-                    <site-brand
-                        class="col-span-10 col-start-2 row-start-1 lg:hidden"
-                    />
-                    <button
-                        class="relative z-10 col-start-11 row-start-1 lg:hidden"
-                        aria-label="Close mobile navigation"
-                        @click="uiStore.toggleShowMobileNav(false)"
-                    >
-                        <u-icon
-                            name="i-material-symbols-close-rounded"
-                            class="ml-auto h-10 w-10"
-                        />
-                    </button>
-                    <site-nav
-                        class="col-span-10 col-start-2 row-start-2 mt-8 lg:mt-0 lg:w-full"
-                        ul-classes="flex gap-2 flex-col lg:flex-row lg:gap-4 lg:justify-end"
-                    />
-                </div>
-            </div>
-            <button
-                class="col-start-11 row-start-1 lg:hidden"
-                aria-label="Open mobile navigation"
-                @click="uiStore.toggleShowMobileNav(true)"
-            >
-                <u-icon
-                    name="i-material-symbols-menu"
-                    class="ml-auto h-10 w-10"
-                />
-            </button>
+            <UColorModeButton />
         </div>
     </header>
 </template>
