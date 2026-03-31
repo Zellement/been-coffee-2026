8a1cb027c519e3e9dabea322df06f81790e5eec3
Dan Farrow <dan@zellement.com>
2026-03-31 12:08:00 +0100

    tidy

--- app/components/Organisms/SiteHeader.vue
+++ app/components/Organisms/SiteHeader.vue
@@ -4,10 +4,7 @@
         class="fixed top-0 right-0 left-0 z-50 w-full py-4 transition-transform duration-500"
     >
         <div class="container grid-layout container-px">
-            <div class="relative col-span-full row-start-1 flex flex-col">
-                <SiteBrand />
-                <SiteNav />
-            </div>
+            <div class="relative col-span-full row-start-1 flex flex-col">sdf</div>
         </div>
     </header>
 </template>
