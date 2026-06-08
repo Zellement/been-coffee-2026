7460a0610a569becf5950c2f91fe41c346fef2ae
Dan Farrow <dan@zellement.com>
2026-06-08 09:52:38 +0100

    add gallery

---
+++ app/components/Molecules/ImageGallery.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="grid col-span-full grid-cols-2 overflow-clip sm:grid-cols-6">
+    <div v-for="(item, index) in media" :key="index" :class="getMosaicClass(index)">
+      <video
+        v-if="item.endsWith('.mp4')"
+        loop
+        disablepictureinpicture
+        muted
+        playsinline
+        autoplay
+        class="h-full w-full object-cover"
+      >
+        <source :src="item" type="video/mp4"></source>
+      </video>
+      <NuxtImg
+        v-else
+        :src="item"
+        :alt="`Gallery item ${index + 1}`"
+        sizes="336px 2xs:425px xs:472px sm:560px lg:780px"
+        quality="85"
+        class="h-full w-full object-cover"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const media = ref(
+  Array.from({ length: 22 }, (_, i) => {
+    switch (i) {
+      case 1:
+        return '/gallery/gallery2.mp4'
+      case 10:
+        return '/gallery/gallery11.mp4'
+      case 18:
+        return '/gallery/gallery19.mp4'
+      default:
+        return `/gallery/gallery${i + 1}.jpg`
+    }
+  })
+)
+
+const getMosaicClass = (index: number) => {
+  const classes = [
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-1 sm:col-span-1 col-span-full',
+    'sm:row-span-1 sm:col-span-1 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-1 sm:col-span-2 col-span-full',
+    'sm:row-span-1 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-1 sm:col-span-1 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-1 col-span-full',
+    'sm:row-span-2 sm:col-span-1 col-span-full',
+    'sm:row-span-3 sm:col-span-2 col-span-full',
+    'sm:row-span-1 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-1 col-span-full',
+    'sm:row-span-2 sm:col-span-1 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+    'sm:row-span-1 sm:col-span-3 col-span-full',
+    'sm:row-span-1 sm:col-span-2 col-span-full',
+    'sm:row-span-2 sm:col-span-2 col-span-full',
+  ]
+  return classes[index % classes.length]
+}
+</script>
---
+++ app/pages/gallery.vue
@@ -0,0 +1,22 @@
+<template>
+  <UPage>
+    <HeroStandard title="Gallery" />
+
+    <div class="container container-px py-4 grid lg:grid-cols-12 lg:gap-10">
+      <ImageGallery />
+    </div>
+  </UPage>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Gallery - Been Coffee image and video gallery',
+  meta: [
+    {
+      name: 'description',
+      content:
+        "Been Coffee's second location, boasting great coffee, delicious food and a warm welcome in the heart of Long Eaton, Nottinghamshire.",
+    },
+  ],
+})
+</script>
