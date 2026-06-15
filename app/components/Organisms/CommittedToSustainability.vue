df07a4209084b415d1b90682571ae1b1c5bf0eaa
Dan Farrow <dan@zellement.com>
2026-06-15 08:37:37 +0100

    more para

--- app/components/Organisms/CommittedToSustainability.vue
+++ app/components/Organisms/CommittedToSustainability.vue
@@ -14,12 +14,27 @@
         outline-class="text-butterscotch-500"
       />
     </h2>
-    <p class="text-center max-w-160 mx-auto text-lg font-krete">
-      We choose brands and partners who share our
-      <span class="text-butterscotch-500">commitment to sustainability</span>, and we're always
-      looking for ways to reduce our environmental impact.
-    </p>
+    <div class="grid grid-cols-1 text-center lg:grid-cols-12 gap-4 lg:text-left lg:gap-12">
+      <p class="max-w-160 mx-auto text-lg font-krete lg:col-span-4">
+        We choose brands and partners who share our
+        <span class="text-butterscotch-500">commitment to sustainability</span>, and we're always
+        looking for ways to reduce our environmental impact.
+      </p>
+      <div class="flex flex-col gap-2 lg:col-start-5 lg:col-span-7">
+        <p>
+          We strive to make everything as environmentally friendly and sustainable as possible. Our
+          takeaway cups are compostable (even the lids!), our pre-packed drinks are all in cans (no
+          plastic!) and we strive to choose suppliers that share our vision for a greener future.
+        </p>
+        <p>
+          When we opened in late 2021, we decided we wanted to prioritise helping our environment as
+          much as a small business can, by choosing to use sustainably sourced ingredients and
+          reduce the amount of non-recyclable waste we were producing.
+        </p>
+      </div>
+    </div>
     <UCarousel
+      v-slot="{ item }"
       class="sm:mx-20"
       :ui="{
         item: 'basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/6 aspect-square',
@@ -27,7 +42,6 @@
       arrows
       loop
       dots
-      v-slot="{ item }"
       :items="imagery"
     >
       <div
