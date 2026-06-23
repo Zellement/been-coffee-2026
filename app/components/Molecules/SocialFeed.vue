350946c5420b67aba9d36ebad018cc0320a39c63
Dan Farrow <dan@zellement.com>
2026-06-23 10:03:30 +0100

    fix use head

--- app/components/Molecules/SocialFeed.vue
+++ app/components/Molecules/SocialFeed.vue
@@ -9,15 +9,10 @@
 useHead({
   script: [
     {
-      innerHTML: `
-        /* curator-feed-default-feed-layout */
-        (function(){
-          var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
-          i.src="https://cdn.curator.io/published/dc1df53f-35a5-47ba-a040-e6a8037907d0.js";
-          e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
-        })();
-      `,
-      type: 'text/javascript',
+      src: 'https://cdn.curator.io/published/dc1df53f-35a5-47ba-a040-e6a8037907d0.js',
+      async: true,
+      crossorigin: 'anonymous',
+      tagPosition: 'bodyClose',
     },
   ],
 })
